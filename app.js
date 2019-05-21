c7c19c0733396f7dd0207bc53ae4d5283463559a
thechiefalone <alperenozkan@gmail.com>
2019-05-21 10:35:25 +0200

    mongodb added

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require('express');
 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+const user ={
+    username: 'alperen',
+    password: 'ozkan'
+}
+
+
+const MongoClient = require('mongodb').MongoClient;
+const dbUrl = 'mongodb+srv://admin:24320124@herokuapp-ohvst.mongodb.net/test?retryWrites=true'
+const dbname = 'prototypeDB';
+
 app.get('/', (req,res)=>{
-    res.send('I am Alperen and welcome to my Heroku Project')
+    (async function mongo(){
+        let client;
+        try{
+            client = await MongoClient.connect(dbUrl, {useNewUrlParser: true});
+            const db = client.db(dbName);
+            const response = await db.collection('users').insertOne(user);
+            res.send(response);
+        }catch(error){
+            res.send(error.message);
+        }
+        client.close();
+    }());
 });
 
 
