00406972b6583c935fe9f544eda0b197a784f448
thechiefalone <alperenozkan@gmail.com>
2019-05-21 16:07:03 +0200

    pages uploaded and index page is running

--- app.js
+++ app.js
@@ -1,83 +1,21 @@
 const express = require('express');
+const path = require('path');
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const user = {
-    username: 'alperen11234',
-    password: 'ozkan1'
-}
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './src/views'));
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 
-const MongoClient = require('mongodb').MongoClient;
-//const dbUrl = 'mongodb+srv://admin:24320124@herokuapp-ohvst.mongodb.net/test?retryWrites=true'
-const dbUrl = 'mongodb://localhost:27017';
-const dbName = 'prototypeDB';
-
-const checkUser = require('./modules/checkUser');
-
-app.get('/', (req, res) => {
-    checkUser(user.username, (check) => {
-        if (check) {
-            (async function mongo() {
-                let client;
-                try {
-                    client = await MongoClient.connect(dbUrl, {
-                        useNewUrlParser: true
-                    });
-                    const db = client.db(dbName);
-                    const response = await db.collection('users').insertOne(user);
-                    res.send(response);
-                } catch (error) {
-                    res.send(error.message);
-                }
-                client.close();
-            }());
-        } else {
-            res.send(`${user.username} is already exist please choose another one`);
-        }
-    });
+app.get('/', (req,res)=>{
+res.render('index');
 });
 
-app.get('/show', (req, res) => {
-    (async function mongo() {
-        let client;
-        try {
-            client = await MongoClient.connect(dbUrl, {
-                useNewUrlParser: true
-            });
-            const db = client.db(dbName);
-            const col = await db.collection('users');
-            const users = await col.find().toArray();
-            res.json(users);
-        } catch (error) {
-            res.send(error.message);
-        }
-        client.close();
-    }());
-});
 
-app.get('/user', (req, res) => {
-    (async function mongo() {
-        let client;
-        try {
-            client = await MongoClient.connect(dbUrl, {
-                useNewUrlParser: true
-            });
-            const db = client.db(dbName);
-            const col = await db.collection('users');
-            const user = await col.findOne({
-                username: 'alperen'
-            });
-            res.send(user);
-
-        } catch (error) {
-            res.send(error.message);
-        }
-        client.close();
-    }());
-})
 
 
 
---
+++ app2.js
@@ -0,0 +1,86 @@
+const express = require('express');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const user = {
+    username: 'alperen11234',
+    password: 'ozkan1'
+}
+
+
+const MongoClient = require('mongodb').MongoClient;
+//const dbUrl = 'mongodb+srv://admin:24320124@herokuapp-ohvst.mongodb.net/test?retryWrites=true'
+const dbUrl = 'mongodb://localhost:27017';
+const dbName = 'prototypeDB';
+
+const checkUser = require('./modules/checkUser');
+
+app.get('/', (req, res) => {
+    checkUser(user.username, (check) => {
+        if (check) {
+            (async function mongo() {
+                let client;
+                try {
+                    client = await MongoClient.connect(dbUrl, {
+                        useNewUrlParser: true
+                    });
+                    const db = client.db(dbName);
+                    const response = await db.collection('users').insertOne(user);
+                    res.send(response);
+                } catch (error) {
+                    res.send(error.message);
+                }
+                client.close();
+            }());
+        } else {
+            res.send(`${user.username} is already exist please choose another one`);
+        }
+    });
+});
+
+app.get('/show', (req, res) => {
+    (async function mongo() {
+        let client;
+        try {
+            client = await MongoClient.connect(dbUrl, {
+                useNewUrlParser: true
+            });
+            const db = client.db(dbName);
+            const col = await db.collection('users');
+            const users = await col.find().toArray();
+            res.json(users);
+        } catch (error) {
+            res.send(error.message);
+        }
+        client.close();
+    }());
+});
+
+app.get('/user', (req, res) => {
+    (async function mongo() {
+        let client;
+        try {
+            client = await MongoClient.connect(dbUrl, {
+                useNewUrlParser: true
+            });
+            const db = client.db(dbName);
+            const col = await db.collection('users');
+            const user = await col.findOne({
+                username: 'alperen'
+            });
+            res.send(user);
+
+        } catch (error) {
+            res.send(error.message);
+        }
+        client.close();
+    }());
+})
+
+
+
+app.listen(port, () => {
+    console.log(`App is listening on ${port}!`);
+});
