7c985198a51104684b43dff51f7cb9e444f76b60
thechiefalone <alperenozkan@gmail.com>
2019-05-22 11:38:22 +0200

    you dont need to define route for evety single page

--- app.js
+++ app.js
@@ -13,15 +13,6 @@ app.use(express.static(path.join(__dirname, '/public')));
 const pageRoutes = require('./src/routes/pageRoutes');
 
 app.use('/',pageRoutes);
-app.use('/about',pageRoutes);
-app.use('/blog',pageRoutes);
-app.use('/contact',pageRoutes);
-app.use('/listings-single',pageRoutes);
-app.use('/listings',pageRoutes);
-app.use('/register',pageRoutes);
-app.use('/login',pageRoutes);
-
-
 
 
 
